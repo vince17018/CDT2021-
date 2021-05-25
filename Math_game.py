078a8e24b52c0f16606d743ad3b2e9c5b14374f8
vince17018 <vince17018@waitakerecollege.school.nz>
2021-05-26 10:05:40 +1200

    Update Math_game.py
    added comments to my existing code

--- Math_game.py
+++ Math_game.py
@@ -4,21 +4,23 @@ import tkinter as tk
 HEIGHT = 720
 WIDTH = 1280
 
-# 
+# Main App
 class Tketris(tk.Tk):
   def __init__(self):
+    # Sets up program and switches the frame to mainMenu
     tk.Tk.__init__(self)
     self._frame = None
     self.switch_frame(mainMenu)
     self.title("Tk.etris")
 
+    # Defines the switch frame function
   def switch_frame(self,frame_class):
     new_frame = frame_class(self)
     if self._frame is not None:
         self._frame.destroy()
     self._frame = new_frame
     self._frame.pack()
-
+# Main Menu window
 class mainMenu(tk.Frame):
   def __init__(self, master):
       tk.Frame.__init__(self, master)
@@ -31,28 +33,32 @@ class mainMenu(tk.Frame):
                 command=lambda: master.switch_frame(Leaderboard)).pack()
       tk.Button(self, text="Quit",
                 command=lambda: self.close_window()).pack()
+  # Quit button function
   def close_window(self):
     self.master.destroy()
 
+# Tetris main window
 class Game(tk.Frame):
     def __init__(self, master):
         tk.Frame.__init__(self, master)
         tk.Label(self, text="Game here").pack(side="top", fill="x", pady=10)
         tk.Button(self, text="Return to main menu",
                   command=lambda: master.switch_frame(mainMenu)).pack()
-
+# Option Menu
 class Options(tk.Frame):
     def __init__(self, master):
         tk.Frame.__init__(self, master)
         tk.Label(self, text="Options Page").pack(side="top", fill="x", pady=10)
         tk.Button(self, text="Return to main menu",
                   command=lambda: master.switch_frame(mainMenu)).pack()
+# Leaderboard
 class Leaderboard(tk.Frame):
     def __init__(self, master):
         tk.Frame.__init__(self, master)
         tk.Label(self, text="Leaderboard Page").pack(side="top", fill="x", pady=10)
         tk.Button(self, text="Return to main menu",
                   command=lambda: master.switch_frame(mainMenu)).pack()
+                  
 def main():
   root = Tketris()
   root.mainloop()
