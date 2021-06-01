83e288d56257e6107bbb90e4beff08e4e1a14b67
vince17018 <vince17018@waitakerecollege.school.nz>
2021-06-01 14:31:26 +1200

    Update timer_test.py

--- Tests and Trials/timer_test.py
+++ Tests and Trials/timer_test.py
@@ -7,8 +7,9 @@ class ExampleApp(tk.Tk):
         self.label.pack()
         self.remaining = 0
         self.countdown(10)
-        
-        self.button = tk.Button(self, text="Stop",command=lambda:self.cancelcountdown()).pack()
+        self.button = tk.Button(self, text="Stop",command=lambda:self.cancelcountdown())
+        self.resetbutton = tk.Button(self,text="Reset",command=lambda:self.reset())
+        self.button.pack()
 
     def countdown(self, remaining = None):
         if remaining is not None:
@@ -16,19 +17,29 @@ class ExampleApp(tk.Tk):
 
         if self.remaining <= 0:
             self.label.configure(text="time's up!")
+            self.button.pack_forget()
+            self.resetbutton.pack()
         else:
-            self.label.configure(text="%d" % self.remaining)
-            self.remaining = self.remaining - 1
-            self.countingdown = self.after(1000, self.countdown)
+            self.label.configure(text="%g" % self.remaining)
+            self.remaining = self.remaining - 0.001
+            self.countingdown = self.after(1, self.countdown)
+
     def cancelcountdown(self):
         self.after_cancel(self.countingdown)
-    
-    
+        self.button.config(text='Continue',command=lambda:self.startcountdown())
+        self.resetbutton.pack()
+        
 
+    def startcountdown(self):
+        self.countdown(self.remaining)
+        self.resetbutton.pack_forget()
+        self.button.config(text='Stop',command=lambda:self.cancelcountdown())
 
-
-
-
+    def reset(self):
+        self.resetbutton.pack_forget()
+        self.button.config(text='Stop',command=lambda:self.cancelcountdown())
+        self.button.pack()
+        self.countdown(10)
 if __name__ == "__main__":
     app = ExampleApp()
     app.mainloop()
